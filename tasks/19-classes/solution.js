eb02c5e30092ee66f7a61f590b155edb93e0be2b
tkhanhq-droid <t.khanh.q@gmail.com>
2026-09-13 02:01:29 -0400

    feature: task 19

--- tasks/19-classes/solution.js
+++ tasks/19-classes/solution.js
@@ -1,6 +1,36 @@
 class Calculator {
-  // TODO: constructor(result=0), add, subtract, multiply, divide, reset, getResult
-  // Methods should be chainable except getResult
+  constructor(result = 0) {
+    this.result = result;
+  }
+
+  add(value) {
+    this.result += value;
+    return this;
+  }
+
+  subtract(value) {
+    this.result -= value;
+    return this;
+  }
+
+  multiply(value) {
+    this.result *= value;
+    return this;
+  }
+
+  divide(value) {
+    this.result /= value;
+    return this;
+  }
+
+  reset() {
+    this.result = 0;
+    return this;
+  }
+
+  getResult() {
+    return this.result;
+  }
 }
 
 class EventEmitter {
@@ -12,20 +42,77 @@ class LinkedList {
 }
 
 class Stack {
-  // TODO: push(value), pop(), peek(), isEmpty(), get size()
+  constructor() {
+    this.items = [];
+  }
+
+  push(value) {
+    this.items.push(value);
+  }
+
+  pop() {
+    return this.items.pop();
+  }
+
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  get size() {
+    return this.items.length;
+  }
 }
 
 class Shape {
-  getArea() { throw new Error('Not implemented'); }
-  getPerimeter() { throw new Error('Not implemented'); }
+  getArea() {
+    throw new Error("Not implemented");
+  }
+  getPerimeter() {
+    throw new Error("Not implemented");
+  }
 }
 
 class Circle extends Shape {
-  // TODO: constructor(radius), getArea(), getPerimeter()
+  constructor(radius) {
+    super();
+    this.radius = radius;
+  }
+
+  getArea() {
+    return Math.PI * this.radius ** 2;
+  }
+
+  getPerimeter() {
+    return 2 * Math.PI * this.radius;
+  }
 }
 
 class Rectangle extends Shape {
-  // TODO: constructor(width, height), getArea(), getPerimeter()
+  constructor(width, height) {
+    super();
+    this.width = width;
+    this.height = height;
+  }
+
+  getArea() {
+    return this.width * this.height;
+  }
+
+  getPerimeter() {
+    return 2 * (this.width + this.height);
+  }
 }
 
-module.exports = { Calculator, EventEmitter, LinkedList, Stack, Shape, Circle, Rectangle };
+module.exports = {
+  Calculator,
+  EventEmitter,
+  LinkedList,
+  Stack,
+  Shape,
+  Circle,
+  Rectangle,
+};
